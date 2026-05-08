37a3f7a6b0d6a6604c14ba38c90c665d8bd99101
raksha pabba <rakshapabba.16@gmail.com>
2026-05-08 11:16:20 +0530

    Fixed API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,11 @@
-
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://team-task-manager-production-3d5e.up.railway.app'
+  baseURL: 'https://team-task-manager-production-3d5e.up.railway.app/api'
 });
 
 API.interceptors.request.use((req) => {
+
   const token = localStorage.getItem('token');
 
   if (token) {
@@ -13,6 +13,7 @@ API.interceptors.request.use((req) => {
   }
 
   return req;
+
 });
 
 export default API;
